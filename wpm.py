3fec415655a446987172a59992e74d6060fc7d84
ImRealKami <123378516+ImRealKami@users.noreply.github.com>
2024-04-15 17:40:15 +0530

    wpm.py

--- wpm.py
+++ wpm.py
@@ -7,8 +7,8 @@ import random
 
 def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("This is the Speed Typing Test! Get ready to type fast")
-    stdscr.addstr("\nPress any key to start")
+    stdscr.addstr("This is the Speed Typing Test! Get ready to type fast", curses.color_pair(4))
+    stdscr.addstr("\nPress any key to start", curses.color_pair(4))
     stdscr.refresh()
     stdscr.getkey()
 
@@ -65,12 +65,17 @@ def wpm_test(stdscr):
         
         elif len(current_text) < len(target_text):
             current_text.append(key)
+        
+        elif len(current_text) == len(target_text):
+            break
+        wpm = 0
     
 def main(stdscr): #standard output screen
 
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
     while True:
